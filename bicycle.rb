37451253960c9357334532168df1af275b2ed168
Booneteam <boone.garrett@gmail.com>
2013-04-16 14:56:14 -0500

    5 pgs left in Ch7

--- bicycle.rb
+++ bicycle.rb
@@ -879,7 +879,688 @@ end
 	
 # Chapter 6 Acquiring behavior through inheritance
 
+# Inheritance at its core is a mechanism for automatic message delegation. 
+# When to use inheritance
+	# -- 
+
+=begin
+class Bicycle
+	attr_reader :size, :tape_color
+
+	def initialize(args)
+		@size 		= args[:size]
+		@tape_color = args[:tape_color]
+	end
+
+	def spares
+		#normally should not put defaults in the method. But ok for this example.
+		{ chain: "10-speed",
+	      tire_size: "23",
+	      tape_color: tape_color }
+	end
+
+end
+
+bike = Bicycle.new(
+	size: 'M',
+	tape_color: 'red')
+p bike.size
+p bike.spares
+=end
 
 
+#Structuring code as below can have many negative consequences. 
+# -- if add new style the if statement must change.
+# -- careless code where last option is default an unexpected style could do something that is not expected.
+# -- some strings are now duplicated in the code for the defaults "10-speed"
+# Think of the class of an object as a specifica case of an attribute that holds the category of self.
+# **Inheritance provides a way to define two objects as having a relationship such that when the first receives a message
+# that it does not understand it automatically forwards the message to the second.
+=begin
+class Bicycle
+	attr_reader  :style, :size, :tape_color, :front_shock, :rear_shock
+
+	def initialize(args)
+		@style 		= args[:style]
+		@size 		= args[:size]
+		@tape_color = args[:tape_color]
+		@front_shock= args[:front_shock]
+		@rear_shock = args[:rear_shock]
+	end
+
+	def spares
+		#normally should not put defaults in the method. But ok for this example.
+		if style == :road
+		{ chain: "10-speed",
+	      tire_size: "23",
+	      tape_color: tape_color }
+	  else
+	  	{ chain: "10-speed",
+	  		tire_size: "2.1",
+	  		rear_shock: rear_shock }
+	  	end
+	end
+end
+
+bike = Bicycle.new(
+	style: :mountain,
+	front_shock: "Minotaur",
+	rear_shock: "Fox",
+	size: 'M',
+	tape_color: 'red')
+p bike.size
+p bike.spares
+=end
+
+=begin
+class Bicycle
+	attr_reader  :style, :size, :tape_color, :front_shock, :rear_shock
+
+	def initialize(args)
+		@style 		= args[:style]
+		@size 		= args[:size]
+		@tape_color = args[:tape_color]
+		@front_shock= args[:front_shock]
+		@rear_shock = args[:rear_shock]
+	end
+
+	def spares
+		#normally should not put defaults in the method. But ok for this example.
+		{ chain: "10-speed",
+	      tire_size: "23",
+	      tape_color: tape_color }
+	end
+end
+# this now creates some behavior taht doesnt make sense such as inheriting the defaults for the original road bike taht do not work with mtn bikes
+
+class MountainBike < Bicycle
+	attr_reader :front_shock, :rear_shock
+
+	def intialize(args)
+		@front_shock = args[:front_shock]
+		@rear_shock = args[:rear_shock]
+		super(args)
+	end
+	def spares
+		super.merge(rear_shock: rear_shock)
+	end
+end
+mtnbike = MountainBike.new(
+	front_shock: "Minotaur",
+	rear_shock: "Fox",
+	size: 'M',)
+p mtnbike.size
+p mtnbike.spares
+=end
+
+# for inheritance to work two rules must be followed
+# -- objects taht you are modeling must truly havea generalization-specialization relationship.
+# -- you must use the correct coding techniques
+
+=begin
+class Bicycle 
+	attr_reader :size, :chain, :tire_size
+
+	def initialize(args={})
+		@size = args[:size]
+		@chain = args[:chain]
+		@tire_size = args[:tire_size]
+	end
+
+	def spares
+		{ chain: "10-speed",
+	      tire_size: "23" }
+	 end
+end
+# It is easier to move the entire bicycle class into roadbike and then promote back up to bicycle so that no artifacts are left behind.
+#RoadBike now inherits the size method frim bicycle. When roadbike receives size Ruby dlegates the message up the superclass chain.
+class RoadBike < Bicycle
+	attr_reader  :tape_color
+
+	def initialize(args)
+		@tape_color = args[:tape_color]
+		super(args)
+	end
+
+	def spares
+		#normally should not put defaults in the method. But ok for this example.
+		{ 
+	      tape_color: tape_color }
+	end
+end
+# this now creates some behavior taht doesnt make sense such as inheriting the defaults for the original road bike taht do not work with mtn bikes
+
+class MountainBike < Bicycle
+	attr_reader :front_shock, :rear_shock
+
+	def intialize(args)
+		@front_shock = args[:front_shock]
+		@rear_shock = args[:rear_shock]
+		super(args)
+	end
+	def spares
+		super.merge(rear_shock: rear_shock)
+	end
+end
+rdbike = RoadBike.new(
+	tape_color: "Red",
+	size: 'M',)
+p rdbike.size
+p rdbike.spares
+=end
 
 
+=begin
+class Bicycle 
+	attr_reader :size, :chain, :tire_size
+	# However, now that there is a  default_tire_size needed as a method we need to document that this must be inside every new bicycle subclass.
+	# You need to implement a default_tire_size raise NotImplementedError.
+
+	def initialize(args={})
+		@size = args[:size]
+		@chain = args[:chain] || default_chain
+		@tire_size = args[:tire_size] || default_tire_size
+	end
+
+	
+	def spares
+		{ chain: "10-speed",
+	      tire_size: "23" }
+	end
+
+	def default_chain
+		'10-speed'
+	end
+
+	#* This provides documentation for later users of the program that they need a default_tire_size method.
+	#While you may rely on only raising the error to track down this error you should add additional documentation.
+	#Attention to detail marks you as a serious programmer
+	def default_tire_size
+		raise NotImplementedError,
+			"This #{self.class} cannot respond to:"
+	end
+end
+# It is easier to move the entire bicycle class into roadbike and then promote back up to bicycle so that no artifacts are left behind.
+#RoadBike now inherits the size method frim bicycle. When roadbike receives size Ruby dlegates the message up the superclass chain.
+class RoadBike < Bicycle
+	attr_reader  :tape_color
+
+	def initialize(args)
+		@tape_color = args[:tape_color]
+		super(args)
+	end
+
+	def default_tire_size 
+		'23'
+	end
+
+	def spares
+		#normally should not put defaults in the method. But ok for this example.
+		{ 
+	      tape_color: tape_color }
+	end
+end
+# this now creates some behavior taht doesnt make sense such as inheriting the defaults for the original road bike taht do not work with mtn bikes
+
+class MountainBike < Bicycle
+	attr_reader :front_shock, :rear_shock
+
+	def intialize(args)
+		@front_shock = args[:front_shock]
+		@rear_shock = args[:rear_shock]
+		super(args)
+	end
+
+	def default_tire_size
+		'2.1'
+	end
+
+	def spares
+		super.merge(rear_shock: rear_shock)
+	end
+end
+
+class RecumbentBike < Bicycle
+	def default_chain
+		'9-speed'
+	end
+end
+rdbike = RoadBike.new(
+	tape_color: "Red",
+	size: 'M',)
+p rdbike.size
+p rdbike
+
+bent = RecumbentBike.new
+=end
+
+ # This hieararchy below workds however, it still contains a booby trap.
+ # B/c both bikes know things about themselves and their superclass these become dependencies
+ # If someone creates a new subclass and forgets to send super in the initialize then nothing will get initialized except for the inputs by the user. All defaults will be nil.
+ # This can also happen if super is missed in the spares method and the hash will be wrong. No one will notice this until it is too late.
+ # This new example adds spares to Bicycle so that MountainBike can now use the spares can be implemented by its superclass and merged with its own spares.
+=begin
+class Bicycle 
+	attr_reader :size, :chain, :tire_size
+
+	def initialize(args={})
+		@size = args[:size]
+		@chain = args[:chain] || default_chain
+		@tire_size = args[:tire_size] || default_tire_size
+	end
+
+	def spares
+		{ chain: chain,
+	      tire_size: tire_size }
+	end
+
+	def default_chain
+		'10-speed'
+	end
+
+	def default_tire_size
+		raise NotImplementedError,
+			"This #{self.class} cannot respond to:"
+	end
+end
+class RoadBike < Bicycle
+	attr_reader  :tape_color
+
+	def initialize(args)
+		@tape_color = args[:tape_color]
+		super(args)
+	end
+
+	def default_tire_size 
+		'23'
+	end
+	# Changed this to mirror mountain bikes to bring RoadBike along with the spares in Bicycle.
+	def spares
+		#normally should not put defaults in the method. But ok for this example.
+	      super.merge({tape_color: tape_color})
+	end
+end
+
+class MountainBike < Bicycle
+	attr_reader :front_shock, :rear_shock
+
+	def initialize(args)
+		@front_shock = args[:front_shock]
+		@rear_shock = args[:rear_shock]
+		super(args)
+	end
+
+	def default_tire_size
+		'2.1'
+	end
+
+	def spares
+		super.merge({rear_shock: rear_shock})
+	end
+end
+
+class RecumbentBike < Bicycle
+	def default_chain
+		'9-speed'
+	end
+end
+rdbike = RoadBike.new(
+	tape_color: "Red",
+	size: 'M')
+p rdbike.size
+p rdbike.spares
+
+mtnbike = MountainBike.new(
+	front_shock: "Fox",
+	size: "L",
+	rear_shock: "Mongoose"
+	)
+p mtnbike
+
+rcmbike = RecumbentBike.new
+
+=end
+
+# All of the problems with the above implementation can be avoided using hooks.This removes knowledge of algorithm from sublcasses and returns control to superclass.
+# These hooks exists solely to provide subclasses a place to contribute information by implementing matching methods.
+
+=begin
+
+class Bicycle 
+	attr_reader :size, :chain, :tire_size
+
+	def initialize(args={})
+		@size = args[:size]
+		@chain = args[:chain] || default_chain
+		@tire_size = args[:tire_size] || default_tire_size
+		post_initialize(args) #bicycle sends this and implements 
+	end
+	#This
+	# This change doesnt remove only the send of super but removes the initialize method altogether from subclasses.
+	# subclasses may override
+	# Subclasses are still responsible for what they initialize but not when it occurs.
+	def post_initialize(args)
+		nil
+	end
+
+	def spares
+		{ chain: chain,
+	      tire_size: tire_size }.merge(local_spares)
+	end
+	# by adding local spares instead of the call to super this decouples them further and adds a hook. 
+	# Bicycle provides a default implementation that returns an empty hash. RoadBike overrides it to return in own version of local_spares.
+	def local_spares
+		{}
+	end
+
+	def default_chain
+		'10-speed'
+	end
+
+	def default_tire_size
+		raise NotImplementedError,
+			"This #{self.class} cannot respond to:"
+	end
+end
+
+class RoadBike < Bicycle
+	attr_reader  :tape_color
+
+	def post_initialize(args)
+		@tape_color = args[:tape_color]
+	end
+
+	def default_tire_size 
+		'23'
+	end
+	
+	def local_spares
+	      {tape_color: tape_color}
+	end
+end
+
+class MountainBike < Bicycle
+	attr_reader :front_shock, :rear_shock
+
+	def post_initialize(args)
+		@front_shock = args[:front_shock]
+		@rear_shock = args[:rear_shock]
+	end
+
+	def default_tire_size
+		'2.1'
+	end
+
+	def local_spares
+		{rear_shock: rear_shock}
+	end
+end
+
+class RecumbentBike < Bicycle
+	attr_reader :flag
+
+	def post_initialize(args)
+		@flag = args[:flag]
+	end
+
+	def local_spares
+		{flag: flag}
+	end
+
+	def default_chain
+		'9-speed'
+	end
+
+	def default_tire_size
+		"28"
+	end
+end
+rdbike = RoadBike.new(
+	tape_color: "Red",
+	size: 'M')
+p rdbike.size
+p rdbike.spares
+
+mtnbike = MountainBike.new(
+	front_shock: "Fox",
+	size: "L",
+	rear_shock: "Mongoose"
+	)
+p mtnbike
+p mtnbike.spares
+
+rcmbike = RecumbentBike.new(flag: 'orange and pretty')
+p rcmbike
+p rcmbike.spares
+
+=end
+
+# Chapter 7 Sharing Role Behavior with Modules
+# before using classical inheritance to solve all of lifes problems think about what happens when you need a recumbent mountain bike! ahh!
+# This is just one example of how inheritance can go awry.
+# Always thing of costs and benefits of each coding style.
+
+# Finding Roles
+ # The preparer duck type from chapter 5 is role. Objects that implement preparers interface play this role. 
+ # The existence of a preparer role shows that there is also a parallel preparable role. The trip class acts as a preparable.
+ # It implements the preparable interface. This includes all the messages that any preparer might expect to send to a preparable.
+
+# Modules
+	# Methods can be defuned in a module and then the module can be added to any object. Modules allow objects of dif lasses to play a common role using a single set of code
+	# Methods defined in a module become available via automatic delegation.
+	# The total set of messages and object can respond includes:
+		# Those it Implements
+		# Those implemented in all objects above it in the hierarchy
+		# Those implemented in any module that has been added to it.
+		# Those implemented in all modules added to any object above it in the hierarchy.
+
+#This code hides the knowledge of who the schedule is and what the schedule does inside of bicycle. Objects holding on to Bicycle no longer need to know about the existence of the Scheduel
+=begin
+class Schedule
+	def scheduled?(schedulable, start_date, end_date)
+		puts "This #{schedulable.class} " + "is not scheduled\n" + " between #{start_date} and #{end_date}" 
+		false
+	end
+end
+
+class Bicycle 
+	attr_reader :size, :chain, :tire_size, :schedule
+
+	def initialize(args={})
+		@schedule = args[:schedule] || Schedule.new # inject schedule and provide default
+		@size = args[:size]
+		@chain = args[:chain] || default_chain
+		@tire_size = args[:tire_size] || default_tire_size ||
+		post_initialize(args) #bicycle sends this and implements 
+	end
+
+	def schedulable?(start_date,end_date)#return true if this bike is available during the (now bike specific) interval
+		!scheduled?(start_date - lead_days, end_date)
+	end
+
+	def scheduled?(start_date,end_date) #return the schedules answer
+		schedule.scheduled?(self, start_date, end_date)
+	end
+	
+	#number of lead days before a bicycle can be scheduled 
+	def lead_days
+		1
+	end
+	#This
+	# This change doesnt remove only the send of super but removes the initialize method altogether from subclasses.
+	# subclasses may override
+	# Subclasses are still responsible for what they initialize but not when it occurs.
+	def post_initialize(args)
+		nil
+	end
+
+	def spares
+		{ chain: chain,
+	      tire_size: tire_size }.merge(local_spares)
+	end
+	# by adding local spares instead of the call to super this decouples them further and adds a hook. 
+	# Bicycle provides a default implementation that returns an empty hash. RoadBike overrides it to return in own version of local_spares.
+	def local_spares
+		{}
+	end
+
+	def default_chain
+		'10-speed'
+	end
+
+	# changed this just to get it to work as an example. 
+	def default_tire_size
+		"40"
+	end
+end
+
+require 'date'
+starting = Date.parse("2015/09/04")
+ending = Date.parse("2015/09/10")
+b = Bicycle.new
+p b.schedulable?(starting,ending)
+
+=end
+
+#This next code extracts teh abstraction so that we can use this same code with Vehicles and Mechanics
+
+module Schedulable
+	attr_writer :schedule
+
+	#this returns an instance of the overall Schedule
+	def schedule
+		@schedule ||= ::Schedule.new
+	end
+
+	def schedulable?(start_date,end_date)
+		!scheduled?(start_date - lead_days, end_date)
+	end
+
+	def scheduled?(start_date,end_date)
+		schedule.scheduled?(self, start_date, end_date)
+	end
+
+	#set at 0 so taht includers may override.
+	def lead_days
+		0
+	end
+
+end
+
+class Schedule
+	def scheduled?(schedulable, start_date, end_date)
+		puts "This #{schedulable.class} " + "is not scheduled\n" + " between #{start_date} and #{end_date}" 
+		false
+	end
+end
+
+class Bicycle
+	include Schedulable 
+	attr_reader :size, :chain, :tire_size
+
+	def initialize(args={})
+		@size = args[:size]
+		@chain = args[:chain] || default_chain
+		@tire_size = args[:tire_size] || default_tire_size
+		post_initialize(args) #bicycle sends this and implements 
+	end
+	#This
+	# This change doesnt remove only the send of super but removes the initialize method altogether from subclasses.
+	# subclasses may override
+	# Subclasses are still responsible for what they initialize but not when it occurs.
+	def post_initialize(args)
+		nil
+	end
+
+	def spares
+		{ chain: chain,
+	      tire_size: tire_size }.merge(local_spares)
+	end
+	# by adding local spares instead of the call to super this decouples them further and adds a hook. 
+	# Bicycle provides a default implementation that returns an empty hash. RoadBike overrides it to return in own version of local_spares.
+	def local_spares
+		{}
+	end
+
+	def default_chain
+		'10-speed'
+	end
+
+	def default_tire_size
+		raise NotImplementedError,
+			"This #{self.class} cannot respond to:"
+	end
+
+	def lead_days
+		1
+	end
+end
+
+class Vehicle
+	include Schedulable
+
+	def lead_days
+		3
+	end
+end
+
+class Mechanic
+	include Schedulable
+
+	def lead_days
+		4 
+	end
+end
+
+
+class MountainBike < Bicycle
+	attr_reader :front_shock, :rear_shock
+
+	def post_initialize(args)
+		@front_shock = args[:front_shock]
+		@rear_shock = args[:rear_shock]
+	end
+
+	def default_tire_size
+		'2.1'
+	end
+
+	def local_spares
+		{rear_shock: rear_shock}
+	end
+end
+require 'date'
+starting = Date.parse("2015/09/04")
+ending = Date.parse("2015/09/10")
+#b = Bicycle.new
+#b.schedulable?(starting,ending)
+
+v = Vehicle.new
+v.schedulable?(starting, ending)
+
+m = Mechanic.new
+m.schedulable?(starting,ending)
+
+mtnbike = MountainBike.new
+mtnbike.schedulable?(starting,ending)
+
+# It is also possible to add a modules methods to a single object, using the extend keyword. Page 300 of 623 shows a diagram of te hierarchy.
+
+# Writing Inheritable Code.
+	# The usefulness and maintainability of inheritance hierarchies and modules is in direct proportion to the quality of the code.
+		# Recognize Antipatterns.
+			# an object that uses a variable with a name like type or category to determine what message to send to self contains
+			#two highly related but slightly different types. 
+
+			# when sending object checks the class of a receiving object to determin what message to send , you may have overlooked a chance to ducktype
+			# In addition to sharing an interface ducktypes might also share behavior. When they do place the sahred code in a module and include that module in each class or object that 
+			#plays that role.
+
+		# Insist on Abstraction
+			# All code in an abstraction should apply to every class that inherits it.
+			# Superclasses should not contain code that applies to some, but not ak, subclasses.
+			#this restriction applie to modules as well.
+			# sublclasses taht rais an exception lie "does not implement" come close to declaring they are not that thing.
+
+		# Honor the Contract
+			
